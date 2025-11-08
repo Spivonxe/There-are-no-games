d92f95cb049d5fc2f9dd041912656c2441da2793
Moritz Ziegner <moritz.ziegner@student.tugraz.at>
2025-11-08 15:19:33 +0100

    generate_embeddings prepares dataset for use in fine_tuning

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -1,10 +1,29 @@
 import pandas as pd
 from sentence_transformers import InputExample
-
-df = pd.read_csv("steam_games_short.csv")
+import torch
+from torch.utils.data import DataLoader, WeightedRandomSampler
+df = pd.read_csv("steam_games_cleaned.csv")
 df = df.dropna(subset=["short_description", "name", "metacritic_score"])
 
 # Normalize metacritic_score to [0,1]
 min_score = df["metacritic_score"].min()
 max_score = df["metacritic_score"].max()
 df["weight"] = (df["metacritic_score"] - min_score) / (max_score - min_score)
+
+# Create InputExamples
+train_examples = [
+    InputExample(texts=[row["short_description"], row["name"]],
+                 label=1.0)
+    for _, row in df.iterrows()
+]
+
+# Convert weights to tensor
+weights = torch.tensor(df['weight'].values, dtype=torch.float)
+
+# Weighted sampler for DataLoader
+sampler = WeightedRandomSampler(weights=weights, num_samples=len(weights), replacement=True)
+
+def collate_fn(batch):
+    return batch
+train_dataloader = DataLoader(train_examples, batch_size=16, sampler=sampler,collate_fn=collate_fn)
+
