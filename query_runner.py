49fed2444c8790d175824b05fdbe0a8eca2ba95e
Rachel Hold <rhold@student.tugraz.at>
2026-01-13 11:44:17 +0100

    inverted usage of BM25_SET_UP to match the description in README

--- query_runner.py
+++ query_runner.py
@@ -9,9 +9,9 @@ GAME_CSV = "steam_games_cleaned.csv"
 BM25_SET_UP=1
 
 def main():
-    splade_base_retrieval.main(INPUT_CSV, OUTPUT_CSV,GAME_CSV)
-    splade_finetuned_retrieval.main(OUTPUT_CSV, OUTPUT_CSV,GAME_CSV)
-    if(BM25_SET_UP == 0):
+    # splade_base_retrieval.main(INPUT_CSV, OUTPUT_CSV,GAME_CSV)
+    # splade_finetuned_retrieval.main(OUTPUT_CSV, OUTPUT_CSV,GAME_CSV)
+    if(BM25_SET_UP):
         bm25_search.main(INPUT_CSV, OUTPUT_CSV)
 
 if __name__ == "__main__":
