40cc742bda9e679eb1abc5a15c76a046a77c8f69
Moritz Ziegner <moritz.ziegner@student.tugraz.at>
2026-01-13 16:58:33 +0100

    fixed query_runner.py

--- query_runner.py
+++ query_runner.py
@@ -2,17 +2,20 @@ import splade_base_retrieval
 import bm25_search
 import splade_finetuned_retrieval
 
-INPUT_CSV = "queries.csv"
+INPUT_CSV = "InputQueries.csv"
 OUTPUT_CSV = "queries_with_results.csv"
 GAME_CSV = "steam_games_cleaned.csv"
 
-BM25_SET_UP=1
-
+BM25_SET_UP=0
+SPLADE_LOCAL=0 #0 when running splade model on e.g google colab 1 when running locally
 def main():
-    # splade_base_retrieval.main(INPUT_CSV, OUTPUT_CSV,GAME_CSV)
-    # splade_finetuned_retrieval.main(OUTPUT_CSV, OUTPUT_CSV,GAME_CSV)
-    if(BM25_SET_UP):
-        bm25_search.main(INPUT_CSV, OUTPUT_CSV)
-
+    if(SPLADE_LOCAL):
+        splade_base_retrieval.main(INPUT_CSV, OUTPUT_CSV,GAME_CSV)
+        splade_finetuned_retrieval.main(OUTPUT_CSV, OUTPUT_CSV,GAME_CSV)
+        if(BM25_SET_UP):
+            bm25_search.main(INPUT_CSV, OUTPUT_CSV)
+    else:
+        if(BM25_SET_UP):
+            bm25_search.main(INPUT_CSV, OUTPUT_CSV)
 if __name__ == "__main__":
     main()
